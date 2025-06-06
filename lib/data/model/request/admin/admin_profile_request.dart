72b71e1f0b4166986652bc6220274fa1b3402863
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-06-06 22:10:00 +0700

    menambahkan admin profile request model

---
+++ lib/data/model/request/admin/admin_profile_request.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+class AdminProfileRequestModel {
+  final String? name;
+
+  AdminProfileRequestModel({this.name});
+
+  factory AdminProfileRequestModel.fromRawJson(String str) =>
+      AdminProfileRequestModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory AdminProfileRequestModel.fromJson(Map<String, dynamic> json) =>
+      AdminProfileRequestModel(name: json["name"]);
+
+  Map<String, dynamic> toJson() => {"name": name};
+}
