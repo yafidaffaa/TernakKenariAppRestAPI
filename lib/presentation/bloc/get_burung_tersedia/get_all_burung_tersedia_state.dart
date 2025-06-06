eb5c5310260c31a1123cc13651b3d88aa9080144
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-06-06 22:32:22 +0700

    menambahkan state get all burung

---
+++ lib/presentation/bloc/get_burung_tersedia/get_all_burung_tersedia_state.dart
@@ -0,0 +1,18 @@
+part of 'get_all_burung_tersedia_bloc.dart';
+
+@immutable
+sealed class GetAllBurungTersediaState {}
+
+final class GetAllBurungTersediaInitial extends GetAllBurungTersediaState {}
+
+final class GetAllBurungTersediaLoading extends GetAllBurungTersediaState {}
+
+final class GetBurungTersediaLoaded extends GetAllBurungTersediaState {
+  final BurungSemuaTersediaModel burungTersedia;
+  GetBurungTersediaLoaded({required this.burungTersedia});
+}
+
+final class GetBurungTersediaError extends GetAllBurungTersediaState {
+  final String message;
+  GetBurungTersediaError({required this.message});
+}
