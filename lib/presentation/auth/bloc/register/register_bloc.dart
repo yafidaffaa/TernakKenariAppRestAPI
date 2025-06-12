62e8c0c6a9ee40a5fde01c85a69a108ab2864783
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-06-12 11:39:55 +0700

    menambahkan bloc untuk register

--- lib/presentation/auth/bloc/register/register_bloc.dart
+++ lib/presentation/auth/bloc/register/register_bloc.dart
@@ -1,13 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:canary/data/model/request/auth/register_request_model.dart';
+import 'package:canary/data/repository/auth_repository.dart';
 import 'package:meta/meta.dart';
 
 part 'register_event.dart';
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(RegisterInitial()) {
-    on<RegisterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AuthRepository authRepository;
+  RegisterBloc({required this.authRepository}) : super(RegisterInitial()) {
+    on<RegisterRequested>(_onRegisterRequested);
+  }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(RegisterLoading());
+
+    final result = await authRepository.register(event.requestModel);
+
+    result.fold(
+      (l) => emit(RegisterFailure(error: l)),
+      (r) => emit(RegisterSuccess(message: r)),
+    );
   }
 }
--- lib/presentation/auth/bloc/register/register_event.dart
+++ lib/presentation/auth/bloc/register/register_event.dart
@@ -1,4 +1,9 @@
 part of 'register_bloc.dart';
 
-@immutable
 sealed class RegisterEvent {}
+
+class RegisterRequested extends RegisterEvent {
+  final RegisterRequestModel requestModel;
+
+  RegisterRequested({required this.requestModel});
+}
