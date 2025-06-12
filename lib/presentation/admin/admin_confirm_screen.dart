883956986c21add92230ad7766df3bee517554f5
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-06-12 11:42:03 +0700

    menambahkan tampilan admin confirm

---
+++ lib/presentation/admin/admin_confirm_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminConfirmScreen extends StatefulWidget {
+  const AdminConfirmScreen({super.key});
+
+  @override
+  State<AdminConfirmScreen> createState() => _AdminConfirmScreenState();
+}
+
+class _AdminConfirmScreenState extends State<AdminConfirmScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
